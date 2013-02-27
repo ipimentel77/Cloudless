12ea83827e432b067bfe5af3580fea76d7d64ca5
Ivan Pimentel <ipimentel77@gmail.com>
2013-02-27 14:57:00 -0400

    Correcciones de Funcionamiento. -Realiza Operaciones Antes de la ejecucion del formulario. -Interrumpe el Formulario.

--- formseeker.js
+++ formseeker.js
@@ -2,5 +2,24 @@ function formseeker(){
 	var defaults = {
 		
 	};
-	alert("formseeker inicializado");
+	function loadDefaultConfiguration(){
+		$('form').submit(function(event){
+			procesarFormulario(this);
+			return false;
+		});
+	}
+	
+	function procesarFormulario(form){
+		var lista = new Array();
+		$($(form).find('*')).each(function(index,element){
+			var aux = $(element).attr('name');
+			if(aux != undefined){
+				alert('|'+aux+'|');
+				lista.push(aux);
+			}
+		});
+		alert(lista);
+	}
+	
+	loadDefaultConfiguration();
 }
